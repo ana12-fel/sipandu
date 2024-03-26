ce7eaa8b55ce8119c72b0df09ff044f1f5c2dd11
Fellyana <109852697+ana12-fel@users.noreply.github.com>
2024-03-26 13:23:04 +0700

    layout

--- sipandu_app/urls.py
+++ sipandu_app/urls.py
@@ -3,7 +3,9 @@ from .views import *
 from django.contrib.auth import views as auth_views
 
 
-app_name = 'profile'
+app_name = 'sipandu'
 urlpatterns = [
     path('', home.index, name='home'),
+    path('identitas/', profile.identitas, name='identitas'),
+    path('Visi/', profile.Visi, name='Visi'),
 ]
