ed479d2c4d614b43c65a12b73e2a82e59bd24d51
Fellyana <109852697+ana12-fel@users.noreply.github.com>
2024-04-23 14:36:43 +0700

    commitan felly

--- sipandu_app/views/__init__.py
+++ sipandu_app/views/__init__.py
@@ -1,2 +1,8 @@
 from .home import *
 from .profile import *
+from .jurusan import*
+from .program import*
+from .kontak import*
+from .prestasi import*
+from .berita import*
+from .galeri import*
---
+++ sipandu_app/views/berita.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.shortcuts import render,redirect
+from support.support_function import JENJANG, TEMPLATE_NAME
+
+def index_berita(request):
+   return render(request, f'{JENJANG}/{TEMPLATE_NAME}/berita/berita.html', )
+def BursaKerja(request):
+   return render(request, f'{JENJANG}/{TEMPLATE_NAME}/berita/bursa_kerja.html', )
+def detail(request):
+   return render(request, f'{JENJANG}/{TEMPLATE_NAME}/berita/detail.html', )
+def pengumuman(request):
+   return render(request, f'{JENJANG}/{TEMPLATE_NAME}/berita/pengumuman.html', )
+def kegiatan(request):
+   return render(request, f'{JENJANG}/{TEMPLATE_NAME}/berita/kegiatan.html', )
+def bursa(request):
+   return render(request, f'{JENJANG}/{TEMPLATE_NAME}/berita/bursa_kerja.html', )
