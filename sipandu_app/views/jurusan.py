c7ec3618830216b6c64bdd828c79bd44d5808f4a
Fellyana <109852697+ana12-fel@users.noreply.github.com>
2024-04-24 18:00:01 +0700

    update felly

--- sipandu_app/views/jurusan.py
+++ sipandu_app/views/jurusan.py
@@ -4,7 +4,7 @@ from support.support_function import JENJANG, TEMPLATE_NAME
 
 def ipa(request):
    return render(request, f'{JENJANG}/{TEMPLATE_NAME}/jurusan/ipa.html', )
-def ips(request):
-   return render(request, f'{JENJANG}/{TEMPLATE_NAME}/jurusan/ips.html', )
-def bahasa(request):
-   return render(request, f'{JENJANG}/{TEMPLATE_NAME}/jurusan/bahasa.html', )
+# def ips(request):
+#    return render(request, f'{JENJANG}/{TEMPLATE_NAME}/jurusan/ips.html', )
+# def bahasa(request):
+#    return render(request, f'{JENJANG}/{TEMPLATE_NAME}/jurusan/bahasa.html', )
