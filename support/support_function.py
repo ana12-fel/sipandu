2f9088acd664581e83d367d993dfff3a32ca2858
Antika <antikasusmita57@gmail.com>
2024-04-25 14:30:58 +0700

    update tema antika

--- support/support_function.py
+++ support/support_function.py
@@ -1,15 +1,10 @@
 def global_var():
     data = {
-<<<<<<< HEAD
         # 'jenjang': 'smp',
         # 'template_name': 'smp_01'
 
         'jenjang': 'sd',
         'template_name': 'sd_01'
-=======
-        'jenjang': 'smp',
-        'template_name': 'smp_01'
->>>>>>> 8c572642e01f286113ca6e9fb2b062878fc5cfc3
     }
     return data
 
