0ff70a0c0af9cd7876e80b76d03541ebb9cccd95
akhilaphiliaa <113088306+akhilaphiliaa@users.noreply.github.com>
2024-05-03 12:24:13 +0700

    update wilayah

--- sipandu_admin/views/wilayah_view.py
+++ sipandu_admin/views/wilayah_view.py
@@ -1,33 +1,42 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
-from sipandu_app.models import Master_wilayah
+from sipandu_app.models import Master_wilayah, LEVEL_WILAYAH
 from django.urls import reverse
 
 def IndexWilayah(request):
     if request.method == 'POST':
         wilayah_id = request.POST.get('wilayah_id')
         wilayah_kode = request.POST.get('wilayah_kode') 
+        wilayah_nama = request.POST.get('wilayah_nama')
+        wilayah_level = request.POST.get('wilayah_level')
         wilayah_status = request.POST.get('wilayah_status')
-        wilayah_provinsi = request.POST.get ('wilayah_provinsi')
-        wilayah_kabupaten = request.POST.get('wilayah_kabupaten')
-        wilayah_distrik = request.POST.get('wilayah_distrik')
+        wilayah_parent = request.POST.get('wilayah_parent')
 
         dt_wilayah = Master_wilayah.objects.create(
             wilayah_id=wilayah_id,
             wilayah_kode=wilayah_kode,
             wilayah_status=wilayah_status,
-            wilayah_provinsi=wilayah_provinsi,
-            wilayah_kabupaten = wilayah_kabupaten,
-            wilayah_distrik = wilayah_distrik,
+            wilayah_nama=wilayah_nama,
+            wilayah_level=wilayah_level,
+            wilayah_parent=wilayah_parent
         )
 
-        print(wilayah_id,wilayah_kode,wilayah_status,wilayah_provinsi,wilayah_kabupaten, wilayah_distrik)
+        print(wilayah_id, wilayah_kode, wilayah_status, wilayah_parent, wilayah_level)
 
         return redirect('sipandu_admin:index_wilayah')
     
     else:
         data_wilayah = Master_wilayah.objects.all()
-        return render(request, 'admin/master/index_master_wilayah.html', {'data_wilayah': data_wilayah})
+        return render(request, 'admin/master/index_master_wilayah.html', {'data_wilayah': data_wilayah, 'wilayah_level': LEVEL_WILAYAH})
+
+def get_wilayah_by_level(request):
+    if request.is_ajax() and request.method == 'GET':
+        level = request.GET.get('level')
+        if level:
+            wilayah_list = Master_wilayah.objects.filter(wilayah_level=level).values_list('wilayah_nama', flat=True)
+            return JsonResponse(list(wilayah_list), safe=False)
+    return JsonResponse({'error': 'Invalid request'})
+
     
 def edit_wilayah(request, wilayah_id):
     if request.method == 'POST':
