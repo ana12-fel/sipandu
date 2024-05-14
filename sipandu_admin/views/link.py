18c52e17f150fe02fe981fab18d21ac1982ae912
Fellyana <109852697+ana12-fel@users.noreply.github.com>
2024-05-14 17:06:45 +0700

    kontak

--- sipandu_admin/views/link.py
+++ sipandu_admin/views/link.py
@@ -1,95 +1,72 @@
-from django.shortcuts import render, redirect,get_object_or_404
-from django.http import JsonResponse, HttpResponse
-from sipandu_app.models import Data_link,Master_sekolah
-from django.urls import reverse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import JsonResponse
+from sipandu_app.models import Data_link, Master_sekolah
 
-def Indexkontak(request):
+def IndexLink(request):
     if request.method == 'POST':
         link_sekolah = request.POST.get('link_sekolah')
-        nama_link = request.POST.get ('nama_link')
-        link = request.POST.get ('link')
-        
+        nama_link = request.POST.get('nama_link')
+        link = request.POST.get('link')
 
-        
-        dt_link = Data_link.objects.create(kontak=link_sekolah)
+        Data_link.objects.create(
+            link_sekolah=link_sekolah,
+            nama_link=nama_link,
+            link=link
+        )
 
-        print(link_sekolah,nama_link,link)
-
-        dt_kontak = Data_link.objects.create(
-                                                link_sekolah=Master_sekolah.objects.get(sekolah_id=link_sekolah),
-                                                nama_link=nama_link,
-                                                link=link)
-
-        return redirect('sipandu_admin:index_kontak')
+        print(link_sekolah, nama_link, link)
+        return redirect('index_link')
     else:
         dt_link = Data_link.objects.all()
         data_sekolah = Master_sekolah.objects.all()
-        return render(request, 'admin/data/kontak_sekolah.html', {"data_link": dt_link, "data_sekolah" : data_sekolah})
-    
-def TambahKontak(request):
+        return render(request, 'admin/data/link_sekolah.html', {"data_link": dt_link, "data_sekolah": data_sekolah})
+
+def TambahLink(request):
     if request.method == 'POST':
         link_sekolah = request.POST.get('link_sekolah')
-        nama_link = request.POST.get ('nama_link')
-        link = request.POST.get ('link')
-        
+        nama_link = request.POST.get('nama_link')
+        link = request.POST.get('link')
 
-        
-        dt_link = Data_link.objects.create(kontak=link_sekolah)
+        Data_link.objects.create(
+            link_sekolah=link_sekolah,
+            nama_link=nama_link,
+            link=link
+        )
 
-        print(link_sekolah,nama_link,link)
-
-        dt_kontak = Data_link.objects.create(
-                                                link_sekolah=Master_sekolah.objects.get(sekolah_id=link_sekolah),
-                                                nama_link=nama_link,
-                                                link=link)
-
-        return redirect('sipandu_admin:index_kontak')
+        print(link_sekolah, nama_link, link)
+        return redirect('index_link')
     else:
-        dt_link = Data_link.objects.all()
         data_sekolah = Master_sekolah.objects.all()
-        return render(request, 'admin/data/kontak_sekolah.html', {"data_link": dt_link, "data_sekolah" : data_sekolah})
+        return render(request, 'admin/data/tambah_link.html', {"data_sekolah": data_sekolah})
 
-def EditKontak(request):
+def EditLink(request, id_data_link):
+    dt_link = get_object_or_404(Data_link, pk=id_data_link)
+
     if request.method == 'POST':
-        kontak_sekolah = request.POST.get('kontak_sekolah')
-        no_hp = request.POST.get ('no_hp')
-        e_mail = request.POST.get ('e-mail')
-        fb = request.POST.get ('fb')
-        tw = request.POST.get ('tw')
-        ig = request.POST.get ('ig')
+        dt_link.link_sekolah = request.POST.get('link_sekolah')
+        dt_link.nama_link = request.POST.get('nama_link')
+        dt_link.link = request.POST.get('link')
+        dt_link.save()
 
-        
-        dt_kontak = Data_kontak.objects.create(kontak=kontak_sekolah)
-
-        print(kontak_sekolah,no_hp,e_mail,fb,tw,ig)
-
-        dt_kontak = Data_kontak.objects.create(
-                                                kontak_sekolah=Master_sekolah.objects.get(sekolah_id=kontak_sekolah),
-                                                no_hp=no_hp,
-                                                email=e_mail,
-                                                fb=fb,
-                                                tw=tw,
-                                                ig=ig)
-
-        return redirect('sipandu_admin:index_kontak')
+        print(dt_link.link_sekolah, dt_link.nama_link, dt_link.link)
+        return redirect('index_link')
     else:
-        dt_kontak = Data_kontak.objects.all()
         data_sekolah = Master_sekolah.objects.all()
-        return render(request, 'admin/data/kontak_sekolah.html', {"data_kontak": dt_kontak, "data_sekolah" : data_sekolah})
+        return render(request, 'admin/data/edit_link.html', {"data_link": dt_link, "data_sekolah": data_sekolah})
 
-def DeleteKontak(request, id_data_kontak):
+def DeleteLink(request, id_data_link):
     try:
-        dt_galeri = get_object_or_404(Data_kontak, galeri_id=id_data_kontak)
-        dt_galeri.delete()
+        dt_link = get_object_or_404(Data_link, pk=id_data_link)
+        dt_link.delete()
         
         data = {
             'status': 'success',
-            'message': 'Data galeri berhasil dihapus'
+            'message': 'Data link berhasil dihapus'
         }
         return JsonResponse(data, status=200)
-    except Data_kontak.DoesNotExist:
+    except Data_link.DoesNotExist:
         data = {
             'status': 'error',
-            'message': 'Data galeri gagal dihapus, data galeri tidak ditemukan'
+            'message': 'Data link tidak ditemukan'
         }
         return JsonResponse(data, status=400)
