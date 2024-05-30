8323e82ded0aba4611e71f9f527f67a6c1e8edaf
Fellyana <109852697+ana12-fel@users.noreply.github.com>
2024-05-30 15:32:42 +0700

    test

--- sipandu_app/views/profile.py
+++ sipandu_app/views/profile.py
@@ -21,3 +21,21 @@ def datagtk(request):
 def detail_guru(request):
    return render(request, f'{request.jenjang}/{request.template_name}/profile/detail_guru.html', )
 
+def global_var(request):
+    data_konten_nav = dt_konten.objects.filter()
+    data_link_footer = dt_link.objects.filter(link_sekolah = request.sekolah)
+    data_kontak_footer = dt_kontak.objects.filter(kontak_sekolah = request.sekolah)
+    data_siswa_foooter = dt_siswa.objects.filter(siswa_sekolah = request.sekolah)
+
+    if request.sekolah:
+        data_sekolah_header = get_object_or_404(dt_situs, sekolah_id = request.sekolah)
+    else:
+        data_sekolah_header = ''
+    # print('cekcok', request.sekolah, data_link_footer)
+    print(data_sekolah_header)
+    data = {
+         'data_link_footer': data_link_footer,
+         'data_kontak_footer':data_kontak_footer,
+         'data_siswa_footer':data_siswa_foooter,
+         'data_sekolah_header':data_sekolah_header, 
+          }
