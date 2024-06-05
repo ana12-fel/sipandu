a21befb3a61d7671ff9ea012120f7807576026cd
Antika <antikasusmita57@gmail.com>
2024-06-05 14:19:17 +0700

    push sub kategori

--- sipandu_admin/views/kategori_views.py
+++ sipandu_admin/views/kategori_views.py
@@ -77,28 +77,31 @@ def kategoriDelete(request, kategori_id):
         return JsonResponse(data, status=400)
     
 def SubKategori(request):
-     if request.method == 'POST':
+    if request.method == 'POST':
         kategori_id_id = request.POST.get('kategori_uraian')
-        Sub_kategori_uraian = request.POST.get ('sub_kategori_uraian')
-        kategori_tema= request.POST.get ('form_sub')
-        sub_kategori_status = request.POST.get('sub_kategori_status')
+        Sub_kategori_uraian = request.POST.get('sub_kategori_uraian')
+        kategori_tema = request.POST.get('form_sub')
+        
+        # Set default value for sub_kategori_status
+        default_status = True  # atau False, tergantung pada kebutuhan aplikasi Anda
+        sub_kategori_status = request.POST.get('sub_kategori_status', default_status)
 
         data = {}
         try:
-            dt_sub_kategori = Sub_kategori.objects.create(kategori_id_id=kategori_id_id,sub_kategori_uraian=Sub_kategori_uraian, sub_kategori_status=sub_kategori_status)
+            dt_sub_kategori = Sub_kategori.objects.create(kategori_id_id=kategori_id_id, sub_kategori_uraian=Sub_kategori_uraian, sub_kategori_status=sub_kategori_status)
             print('sukses')
             data['status'] = True
-            return JsonResponse(data, status = 201)
+            return JsonResponse(data, status=201)
         except IntegrityError as e:
             print('error insert sub kategori', e)
             data['status'] = False
-            return JsonResponse(data, status = 400)
+            return JsonResponse(data, status=400)
 
-     else:
+    else:
         data_tema = Master_tema.objects.all()
         data_kategori = Master_kategori.objects.all()
         data_sub_kategori = Sub_kategori.objects.all()
-        return render(request, 'admin/master_kategori/master_kategori.html', {'data_tema': data_tema, 'data_kategori' : data_kategori, 'data_sub_kategori' : data_sub_kategori})
+        return render(request, 'admin/master_kategori/master_kategori.html', {'data_tema': data_tema, 'data_kategori': data_kategori, 'data_sub_kategori': data_sub_kategori})
      
 def edit_sub_kategori(request, sub_kategori_id_):
     if request.method == 'POST':
